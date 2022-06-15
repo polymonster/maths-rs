af446818dd3c168f849011041cbfac62448ab461
polymonster <alexandercdixon@gmail.com>
2022-06-15 19:33:14 +0200

    - more tests

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,14 @@ pub mod vec;
 use vec::*;
 
 #[test]
-fn v2_construct_and_equal() {
+fn v2_construct() {
     let v2 = Vec2f {
         x: 2.0,
         y: 3.0
     };
     let v2_short = vec2f(2.0, 3.0);
-    assert_eq!(v2.x, v2_short.x);
-    assert_eq!(v2.y, v2_short.y);
-    assert_eq!(v2, v2_short);
-
-    let v2_wrong = vec2f(3.0, 2.0);
-    assert_ne!(v2.x, v2_wrong.x);
-    assert_ne!(v2.y, v2_wrong.y);
-    assert_ne!(v2, v2_wrong);
-
     let v2_new = Vec2f::new(2.0, 3.0);
-    assert_eq!(v2.x, v2_new.x);
-    assert_eq!(v2.y, v2_new.y);
+    assert_eq!(v2, v2_short);
     assert_eq!(v2, v2_new);
 }
 
@@ -56,6 +46,43 @@ fn v4_construct() {
 #[test]
 fn v4_len() {
     assert_eq!(Vec4f::len(), 4);
+}
+
+#[test]
+fn equal() {
+    let a = Vec2f {
+        x: 2.0,
+        y: 3.0
+    };
+    let b = Vec2f {
+        x: 2.0,
+        y: 3.0
+    };
+    let c = Vec2f {
+        x: 3.0,
+        y: 2.0
+    };
+    assert_eq!(a.x, b.x);
+    assert_eq!(a.y, b.y);
+    assert_eq!(a, b);
+    assert_ne!(a.x, c.x);
+    assert_ne!(a.y, c.y);
+    assert_ne!(a, c);
+}
+
+#[test]
+fn neg() {
+    // neg
+    let v1 = vec2f(10.0, 20.0);
+    let expected = vec2f(-10.0, -20.0);
+    let result = -v1;
+    assert_eq!(result, expected);
+
+    // neg with negative
+    let v1 = vec2f(-10.0, -20.0);
+    let expected = vec2f(10.0, 20.0);
+    let result = -v1;
+    assert_eq!(result, expected);
 }
 
 #[test]
@@ -125,6 +152,117 @@ fn add_assign_scalar() {
     v1 += v2;
     assert_eq!(expected, v1);
 }
+
+#[test]
+fn sub() {
+    // sub
+    let v1 = vec2f(40.0, 90.0);
+    let v2 = vec2f(20.0, 22.0);
+    let expected = vec2f(20.0, 68.0);
+    let result = v1 - v2;
+    assert_eq!(expected, result);
+
+    // sub with negative
+    let v1 = vec2f(31.0, 224.0);
+    let v2 = vec2f(-67.0, -320.0);
+    let expected = vec2f(98.0, 544.0);
+    let result = v1 - v2;
+    assert_eq!(expected, result);
+}
+
+#[test]
+fn sub_assign() {
+    // sub
+    let mut v1 = vec2f(40.0, 90.0);
+    let v2 = vec2f(20.0, 22.0);
+    let expected = vec2f(20.0, 68.0);
+    v1 -= v2;
+    assert_eq!(expected, v1);
+
+    // sub with negative
+    let mut v1 = vec2f(31.0, 224.0);
+    let v2 = vec2f(-67.0, -320.0);
+    let expected = vec2f(98.0, 544.0);
+    v1 -= v2;
+    assert_eq!(expected, v1);
+}
+
+#[test]
+fn sub_scalar() {
+    // sub
+    let v1 = vec2f(276.0, 17.0);
+    let v2 = 112.0;
+    let expected = vec2f(164.0, -95.0);
+    let result = v1 - v2;
+    assert_eq!(expected, result);
+
+    // sub with negative
+    let v1 = vec2f(28.0, 162.0);
+    let v2 = -13.0;
+    let expected = vec2f(41.0, 175.0);
+    let result = v1 - v2;
+    assert_eq!(expected, result);
+}
+
+#[test]
+fn sub_assign_scalar() {
+    // sub
+    let mut v1 = vec2f(276.0, 17.0);
+    let v2 = 112.0;
+    let expected = vec2f(164.0, -95.0);
+    v1 -= v2;
+    assert_eq!(expected, v1);
+
+    // sub with negative
+    let mut v1 = vec2f(28.0, 162.0);
+    let v2 = -13.0;
+    let expected = vec2f(41.0, 175.0);
+    v1 -= v2;
+    assert_eq!(expected, v1);
+}
+
+#[test]
+fn mul() {
+    // mul
+    let v1 = vec2f(16.0, 543.0);
+    let v2 = vec2f(2.0, 10.0);
+    let expected = vec2f(32.0, 5430.0);
+    let result = v1 * v2;
+    assert_eq!(expected, result);
+
+    // mul with negative
+    let v1 = vec2f(18.0, 4.0);
+    let v2 = vec2f(-56.0, -9.0);
+    let expected = vec2f(-1008.0, -36.0);
+    let result = v1 * v2;
+    assert_eq!(expected, result);
+}
+
+#[test]
+fn mul_assign() {
+    // mul
+    let mut v1 = vec2f(16.0, 543.0);
+    let v2 = vec2f(2.0, 10.0);
+    let expected = vec2f(32.0, 5430.0);
+    v1 *= v2;
+    assert_eq!(expected, v1);
+
+    // mul with negative
+    let mut v1 = vec2f(18.0, 4.0);
+    let v2 = vec2f(-56.0, -9.0);
+    let expected = vec2f(-1008.0, -36.0);
+    v1 *= v2;
+    assert_eq!(expected, v1);
+}
+
+// mul scalar
+// mul assign scalar
+
+// div
+// div assign
+
+// div scalar
+// div assign scalar
 
 fn main() {
 }
