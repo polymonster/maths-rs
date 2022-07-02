3bb1e6a1ab515b0ee298c7bccda34a2450f6c6a6
polymonster <alexandercdixon@gmail.com>
2022-07-02 14:01:18 +0100

    - added matrix multiply combinations and mul assigns

--- src/mat.rs
+++ src/mat.rs
@@ -5,6 +5,7 @@ use crate::num::*;
 use std::ops::Index;
 use std::ops::IndexMut;
 use std::ops::Mul;
+use std::ops::MulAssign;
 
 use std::fmt::Display;
 use std::fmt::Formatter;
@@ -93,7 +94,7 @@ macro_rules! mat_impl {
             }
 
             /// gets a single row of the matrix in n sized vec where in is the column count of the matrix
-            pub fn get_row(&self, row: u32) -> $RowVecN<T> {
+            pub fn get_row(self, row: u32) -> $RowVecN<T> {
                 let urow = row as usize;
                 $RowVecN {
                     $($row_field: self.m[urow * $cols + $row_field_index],)+
@@ -107,7 +108,7 @@ macro_rules! mat_impl {
             }
 
             /// gets a single column of the matrix in n sized vec where in is the row count of the matrix
-            pub fn get_column(&self, column: u32) -> $ColVecN<T> {
+            pub fn get_column(self, column: u32) -> $ColVecN<T> {
                 let ucol = column as usize;
                 $ColVecN {
                     $($col_field: self.m[$col_field_index * $cols + ucol],)+
@@ -123,108 +124,231 @@ macro_rules! mat_impl {
     }
 }
 
+// mat2
 // 00 01
 // 02 03
+
+fn mul2x2<T: Number>(lhs: Mat2<T>, rhs: Mat2<T>) -> Mat2<T> {
+    Mat2 {
+        m: [
+            lhs.m[0] * rhs.m[0] + lhs.m[1] * rhs.m[2],
+            lhs.m[0] * rhs.m[1] + lhs.m[1] * rhs.m[3],
+            lhs.m[2] * rhs.m[0] + lhs.m[3] * rhs.m[2],
+            lhs.m[2] * rhs.m[1] + lhs.m[3] * rhs.m[3],
+        ]
+    }
+}
 
 impl<T> Mul<Self> for Mat2<T> where T: Number {
     type Output = Self;
     fn mul(self, rhs: Mat2<T>) -> Mat2<T> {
-        Mat2 {
-            m: [
-                self.m[0] * rhs.m[0] + self.m[1] * rhs.m[2],
-                self.m[0] * rhs.m[1] + self.m[1] * rhs.m[3],
-
-                self.m[2] * rhs.m[0] + self.m[3] * rhs.m[2],
-                self.m[2] * rhs.m[1] + self.m[3] * rhs.m[3],
-            ]
-        }
+        mul2x2(self, rhs)
     }
 }
 
+impl<T> MulAssign<Self> for Mat2<T> where T: Number {
+    fn mul_assign(&mut self, rhs: Mat2<T>) {
+        *self = mul2x2(*self, rhs);
+    }
+}
+
+// mat3
 // 00 01 02
 // 03 04 05
 // 06 07 08
 
-impl<T> Mul<Self> for Mat3<T> where T: Number {
-    type Output = Self;
-    fn mul(self, rhs: Mat3<T>) -> Mat3<T> {
-        Mat3 {
-            m: [
-                self.m[0] * rhs.m[0] + self.m[1] * rhs.m[3] + self.m[2] * rhs.m[6],
-                self.m[0] * rhs.m[1] + self.m[1] * rhs.m[4] + self.m[2] * rhs.m[7],
-                self.m[0] * rhs.m[2] + self.m[1] * rhs.m[5] + self.m[2] * rhs.m[8],
+fn mul3x3<T: Number>(lhs: Mat3<T>, rhs: Mat3<T>) -> Mat3<T> {
+    Mat3 {
+        m: [
+            lhs.m[0] * rhs.m[0] + lhs.m[1] * rhs.m[3] + lhs.m[2] * rhs.m[6],
+            lhs.m[0] * rhs.m[1] + lhs.m[1] * rhs.m[4] + lhs.m[2] * rhs.m[7],
+            lhs.m[0] * rhs.m[2] + lhs.m[1] * rhs.m[5] + lhs.m[2] * rhs.m[8],
 
-                self.m[3] * rhs.m[0] + self.m[4] * rhs.m[3] + self.m[5] * rhs.m[6],
-                self.m[3] * rhs.m[1] + self.m[4] * rhs.m[4] + self.m[5] * rhs.m[7],
-                self.m[3] * rhs.m[2] + self.m[4] * rhs.m[5] + self.m[5] * rhs.m[8],
+            lhs.m[3] * rhs.m[0] + lhs.m[4] * rhs.m[3] + lhs.m[5] * rhs.m[6],
+            lhs.m[3] * rhs.m[1] + lhs.m[4] * rhs.m[4] + lhs.m[5] * rhs.m[7],
+            lhs.m[3] * rhs.m[2] + lhs.m[4] * rhs.m[5] + lhs.m[5] * rhs.m[8],
 
-                self.m[6] * rhs.m[0] + self.m[7] * rhs.m[3] + self.m[8] * rhs.m[6],
-                self.m[6] * rhs.m[1] + self.m[7] * rhs.m[4] + self.m[8] * rhs.m[7],
-                self.m[6] * rhs.m[2] + self.m[7] * rhs.m[5] + self.m[8] * rhs.m[8],
-            ]
-        }
+            lhs.m[6] * rhs.m[0] + lhs.m[7] * rhs.m[3] + lhs.m[8] * rhs.m[6],
+            lhs.m[6] * rhs.m[1] + lhs.m[7] * rhs.m[4] + lhs.m[8] * rhs.m[7],
+            lhs.m[6] * rhs.m[2] + lhs.m[7] * rhs.m[5] + lhs.m[8] * rhs.m[8],
+        ]
     }
 }
 
+impl<T> Mul<Self> for Mat3<T> where T: Number {
+    type Output = Self;
+    fn mul(self, rhs: Mat3<T>) -> Mat3<T> {
+        mul3x3(self, rhs)
+    }
+}
+
+impl<T> MulAssign<Self> for Mat3<T> where T: Number {
+    fn mul_assign(&mut self, rhs: Mat3<T>) {
+        *self = mul3x3(*self, rhs);
+    }
+}
+
+// mat4
 // 00 01 02 03
 // 04 05 06 07
 // 08 09 10 11
 // 12 13 14 15
 
-impl<T> Mul<Self> for Mat4<T> where T: Number {
-    type Output = Self;
-    fn mul(self, rhs: Mat4<T>) -> Mat4<T> {
-        Mat4 {
-            m: [
-                self.m[0] * rhs.m[0] + self.m[1] * rhs.m[4] + self.m[2] * rhs.m[8] + self.m[3] * rhs.m[12],
-                self.m[0] * rhs.m[1] + self.m[1] * rhs.m[5] + self.m[2] * rhs.m[9] + self.m[3] * rhs.m[13],
-                self.m[0] * rhs.m[2] + self.m[1] * rhs.m[6] + self.m[2] * rhs.m[10] + self.m[3] * rhs.m[14],
-                self.m[0] * rhs.m[3] + self.m[1] * rhs.m[7] + self.m[2] * rhs.m[11] + self.m[3] * rhs.m[15],
+fn mul4x4<T: Number>(lhs: Mat4<T>, rhs: Mat4<T>) -> Mat4<T> {
+    Mat4 {
+        m: [
+            lhs.m[0] * rhs.m[0] + lhs.m[1] * rhs.m[4] + lhs.m[2] * rhs.m[8] + lhs.m[3] * rhs.m[12],
+            lhs.m[0] * rhs.m[1] + lhs.m[1] * rhs.m[5] + lhs.m[2] * rhs.m[9] + lhs.m[3] * rhs.m[13],
+            lhs.m[0] * rhs.m[2] + lhs.m[1] * rhs.m[6] + lhs.m[2] * rhs.m[10] + lhs.m[3] * rhs.m[14],
+            lhs.m[0] * rhs.m[3] + lhs.m[1] * rhs.m[7] + lhs.m[2] * rhs.m[11] + lhs.m[3] * rhs.m[15],
 
-                self.m[4] * rhs.m[0] + self.m[5] * rhs.m[4] + self.m[6] * rhs.m[8] + self.m[7] * rhs.m[12],
-                self.m[4] * rhs.m[1] + self.m[5] * rhs.m[5] + self.m[6] * rhs.m[9] + self.m[7] * rhs.m[13],
-                self.m[4] * rhs.m[2] + self.m[5] * rhs.m[6] + self.m[6] * rhs.m[10] + self.m[7] * rhs.m[14],
-                self.m[4] * rhs.m[3] + self.m[5] * rhs.m[7] + self.m[6] * rhs.m[11] + self.m[7] * rhs.m[15],
+            lhs.m[4] * rhs.m[0] + lhs.m[5] * rhs.m[4] + lhs.m[6] * rhs.m[8] + lhs.m[7] * rhs.m[12],
+            lhs.m[4] * rhs.m[1] + lhs.m[5] * rhs.m[5] + lhs.m[6] * rhs.m[9] + lhs.m[7] * rhs.m[13],
+            lhs.m[4] * rhs.m[2] + lhs.m[5] * rhs.m[6] + lhs.m[6] * rhs.m[10] + lhs.m[7] * rhs.m[14],
+            lhs.m[4] * rhs.m[3] + lhs.m[5] * rhs.m[7] + lhs.m[6] * rhs.m[11] + lhs.m[7] * rhs.m[15],
 
-                self.m[8] * rhs.m[0] + self.m[9] * rhs.m[4] + self.m[10] * rhs.m[8] + self.m[11] * rhs.m[12],
-                self.m[8] * rhs.m[1] + self.m[9] * rhs.m[5] + self.m[10] * rhs.m[9] + self.m[11] * rhs.m[13],
-                self.m[8] * rhs.m[2] + self.m[9] * rhs.m[6] + self.m[10] * rhs.m[10] + self.m[11] * rhs.m[14],
-                self.m[8] * rhs.m[3] + self.m[9] * rhs.m[7] + self.m[10] * rhs.m[11] + self.m[11] * rhs.m[15],
+            lhs.m[8] * rhs.m[0] + lhs.m[9] * rhs.m[4] + lhs.m[10] * rhs.m[8] + lhs.m[11] * rhs.m[12],
+            lhs.m[8] * rhs.m[1] + lhs.m[9] * rhs.m[5] + lhs.m[10] * rhs.m[9] + lhs.m[11] * rhs.m[13],
+            lhs.m[8] * rhs.m[2] + lhs.m[9] * rhs.m[6] + lhs.m[10] * rhs.m[10] + lhs.m[11] * rhs.m[14],
+            lhs.m[8] * rhs.m[3] + lhs.m[9] * rhs.m[7] + lhs.m[10] * rhs.m[11] + lhs.m[11] * rhs.m[15],
 
-                self.m[12] * rhs.m[0] + self.m[10] * rhs.m[4] + self.m[14] * rhs.m[8] + self.m[15] * rhs.m[12],
-                self.m[12] * rhs.m[1] + self.m[10] * rhs.m[5] + self.m[14] * rhs.m[9] + self.m[15] * rhs.m[13],
-                self.m[12] * rhs.m[2] + self.m[10] * rhs.m[6] + self.m[14] * rhs.m[10] + self.m[15] * rhs.m[14],
-                self.m[12] * rhs.m[3] + self.m[10] * rhs.m[7] + self.m[14] * rhs.m[11] + self.m[15] * rhs.m[15],
-            ]
-        }
+            lhs.m[12] * rhs.m[0] + lhs.m[13] * rhs.m[4] + lhs.m[14] * rhs.m[8] + lhs.m[15] * rhs.m[12],
+            lhs.m[12] * rhs.m[1] + lhs.m[13] * rhs.m[5] + lhs.m[14] * rhs.m[9] + lhs.m[15] * rhs.m[13],
+            lhs.m[12] * rhs.m[2] + lhs.m[13] * rhs.m[6] + lhs.m[14] * rhs.m[10] + lhs.m[15] * rhs.m[14],
+            lhs.m[12] * rhs.m[3] + lhs.m[13] * rhs.m[7] + lhs.m[14] * rhs.m[11] + lhs.m[15] * rhs.m[15],
+        ]
     }
 }
 
+impl<T> Mul<Self> for Mat4<T> where T: Number {
+    type Output = Self;
+    fn mul(self, rhs: Mat4<T>) -> Mat4<T> {
+        mul4x4(self, rhs)
+    }
+}
+
+impl<T> MulAssign<Self> for Mat4<T> where T: Number {
+    fn mul_assign(&mut self, rhs: Mat4<T>) {
+        *self = mul4x4(*self, rhs);
+    }
+}
+
+// mat34
 // 00 01 02 03
 // 04 05 06 07
 // 08 09 10 11
 
+fn mul3x4<T: Number>(lhs: Mat34<T>, rhs: Mat34<T>) -> Mat34<T> {
+    Mat34 {
+        m: [
+            lhs.m[0] * rhs.m[0] + lhs.m[1] * rhs.m[4] + lhs.m[2] * rhs.m[8],
+            lhs.m[0] * rhs.m[1] + lhs.m[1] * rhs.m[5] + lhs.m[2] * rhs.m[9],
+            lhs.m[0] * rhs.m[2] + lhs.m[1] * rhs.m[6] + lhs.m[2] * rhs.m[10],
+            lhs.m[0] * rhs.m[3] + lhs.m[1] * rhs.m[7] + lhs.m[2] * rhs.m[11] + lhs.m[3],
+
+            lhs.m[4] * rhs.m[0] + lhs.m[5] * rhs.m[4] + lhs.m[6] * rhs.m[8],
+            lhs.m[4] * rhs.m[1] + lhs.m[5] * rhs.m[5] + lhs.m[6] * rhs.m[9],
+            lhs.m[4] * rhs.m[2] + lhs.m[5] * rhs.m[6] + lhs.m[6] * rhs.m[10],
+            lhs.m[4] * rhs.m[3] + lhs.m[5] * rhs.m[7] + lhs.m[6] * rhs.m[11] + lhs.m[7],
+
+            lhs.m[8] * rhs.m[0] + lhs.m[9] * rhs.m[4] + lhs.m[10] * rhs.m[8],
+            lhs.m[8] * rhs.m[1] + lhs.m[9] * rhs.m[5] + lhs.m[10] * rhs.m[9],
+            lhs.m[8] * rhs.m[2] + lhs.m[9] * rhs.m[6] + lhs.m[10] * rhs.m[10],
+            lhs.m[8] * rhs.m[3] + lhs.m[9] * rhs.m[7] + lhs.m[10] * rhs.m[11] + lhs.m[11],
+        ]
+    }
+}
+
 impl<T> Mul<Self> for Mat34<T> where T: Number {
     type Output = Self;
     fn mul(self, rhs: Mat34<T>) -> Mat34<T> {
-        Mat34 {
-            m: [
-                self.m[0] * rhs.m[0] + self.m[1] * rhs.m[4] + self.m[2] * rhs.m[8],
-                self.m[0] * rhs.m[1] + self.m[1] * rhs.m[5] + self.m[2] * rhs.m[9],
-                self.m[0] * rhs.m[2] + self.m[1] * rhs.m[6] + self.m[2] * rhs.m[10],
-                self.m[0] * rhs.m[3] + self.m[1] * rhs.m[7] + self.m[2] * rhs.m[11] + self.m[3],
+        mul3x4(self, rhs)
+    }
+}
 
-                self.m[4] * rhs.m[0] + self.m[5] * rhs.m[4] + self.m[6] * rhs.m[8],
-                self.m[4] * rhs.m[1] + self.m[5] * rhs.m[5] + self.m[6] * rhs.m[9],
-                self.m[4] * rhs.m[2] + self.m[5] * rhs.m[6] + self.m[6] * rhs.m[10],
-                self.m[4] * rhs.m[3] + self.m[5] * rhs.m[7] + self.m[6] * rhs.m[11] + self.m[7],
+impl<T> MulAssign<Self> for Mat34<T> where T: Number {
+    fn mul_assign(&mut self, rhs: Mat34<T>) {
+        *self = mul3x4(*self, rhs)
+    }
+}
 
-                self.m[8] * rhs.m[0] + self.m[9] * rhs.m[4] + self.m[10] * rhs.m[8],
-                self.m[8] * rhs.m[1] + self.m[9] * rhs.m[5] + self.m[10] * rhs.m[9],
-                self.m[8] * rhs.m[2] + self.m[9] * rhs.m[6] + self.m[10] * rhs.m[10],
-                self.m[8] * rhs.m[3] + self.m[9] * rhs.m[7] + self.m[10] * rhs.m[11] + self.m[11],
-            ]
-        }
+// mat34 implicit last row
+// 12 13 14 15
+// 00 00 00 01
+
+fn mul3x4_4x4<T: Number>(lhs: Mat34<T>, rhs: Mat4<T>) -> Mat4<T> {
+    Mat4 {
+        m: [
+            lhs.m[0] * rhs.m[0] + lhs.m[1] * rhs.m[4] + lhs.m[2] * rhs.m[8] + lhs.m[3] * rhs.m[12],
+            lhs.m[0] * rhs.m[1] + lhs.m[1] * rhs.m[5] + lhs.m[2] * rhs.m[9] + lhs.m[3] * rhs.m[13],
+            lhs.m[0] * rhs.m[2] + lhs.m[1] * rhs.m[6] + lhs.m[2] * rhs.m[10] + lhs.m[3] * rhs.m[14],
+            lhs.m[0] * rhs.m[3] + lhs.m[1] * rhs.m[7] + lhs.m[2] * rhs.m[11] + lhs.m[3] * rhs.m[15],
+
+            lhs.m[4] * rhs.m[0] + lhs.m[5] * rhs.m[4] + lhs.m[6] * rhs.m[8] + lhs.m[7] * rhs.m[12],
+            lhs.m[4] * rhs.m[1] + lhs.m[5] * rhs.m[5] + lhs.m[6] * rhs.m[9] + lhs.m[7] * rhs.m[13],
+            lhs.m[4] * rhs.m[2] + lhs.m[5] * rhs.m[6] + lhs.m[6] * rhs.m[10] + lhs.m[7] * rhs.m[14],
+            lhs.m[4] * rhs.m[3] + lhs.m[5] * rhs.m[7] + lhs.m[6] * rhs.m[11] + lhs.m[7] * rhs.m[15],
+
+            lhs.m[8] * rhs.m[0] + lhs.m[9] * rhs.m[4] + lhs.m[10] * rhs.m[8] + lhs.m[11] * rhs.m[12],
+            lhs.m[8] * rhs.m[1] + lhs.m[9] * rhs.m[5] + lhs.m[10] * rhs.m[9] + lhs.m[11] * rhs.m[13],
+            lhs.m[8] * rhs.m[2] + lhs.m[9] * rhs.m[6] + lhs.m[10] * rhs.m[10] + lhs.m[11] * rhs.m[14],
+            lhs.m[8] * rhs.m[3] + lhs.m[9] * rhs.m[7] + lhs.m[10] * rhs.m[11] + lhs.m[11] * rhs.m[15],
+
+            rhs.m[12],
+            rhs.m[13],
+            rhs.m[14],
+            rhs.m[15],
+        ]
+    }
+}
+
+impl<T> Mul<Mat4<T>> for Mat34<T> where T: Number {
+    type Output = Mat4<T>;
+    fn mul(self, rhs: Mat4<T>) -> Mat4<T> {
+        mul3x4_4x4(self, rhs)
+    }
+}
+
+// mat34 implicit last row
+// 12 13 14 15
+// 00 00 00 01
+
+fn mul4x4_3x4<T: Number>(lhs: Mat4<T>, rhs: Mat34<T>) -> Mat4<T> {
+    Mat4 {
+        m: [
+            lhs.m[0] * rhs.m[0] + lhs.m[1] * rhs.m[4] + lhs.m[2] * rhs.m[8],
+            lhs.m[0] * rhs.m[1] + lhs.m[1] * rhs.m[5] + lhs.m[2] * rhs.m[9],
+            lhs.m[0] * rhs.m[2] + lhs.m[1] * rhs.m[6] + lhs.m[2] * rhs.m[10],
+            lhs.m[0] * rhs.m[3] + lhs.m[1] * rhs.m[7] + lhs.m[2] * rhs.m[11] + lhs.m[3],
+
+            lhs.m[4] * rhs.m[0] + lhs.m[5] * rhs.m[4] + lhs.m[6] * rhs.m[8],
+            lhs.m[4] * rhs.m[1] + lhs.m[5] * rhs.m[5] + lhs.m[6] * rhs.m[9],
+            lhs.m[4] * rhs.m[2] + lhs.m[5] * rhs.m[6] + lhs.m[6] * rhs.m[10],
+            lhs.m[4] * rhs.m[3] + lhs.m[5] * rhs.m[7] + lhs.m[6] * rhs.m[11] + lhs.m[7],
+
+            lhs.m[8] * rhs.m[0] + lhs.m[9] * rhs.m[4] + lhs.m[10] * rhs.m[8],
+            lhs.m[8] * rhs.m[1] + lhs.m[9] * rhs.m[5] + lhs.m[10] * rhs.m[9],
+            lhs.m[8] * rhs.m[2] + lhs.m[9] * rhs.m[6] + lhs.m[10] * rhs.m[10],
+            lhs.m[8] * rhs.m[3] + lhs.m[9] * rhs.m[7] + lhs.m[10] * rhs.m[11] + lhs.m[11],
+
+            lhs.m[12] * rhs.m[0] + lhs.m[10] * rhs.m[4] + lhs.m[14] * rhs.m[8],
+            lhs.m[12] * rhs.m[1] + lhs.m[10] * rhs.m[5] + lhs.m[14] * rhs.m[9],
+            lhs.m[12] * rhs.m[2] + lhs.m[10] * rhs.m[6] + lhs.m[14] * rhs.m[10],
+            lhs.m[12] * rhs.m[3] + lhs.m[10] * rhs.m[7] + lhs.m[14] * rhs.m[11] + lhs.m[15],
+        ]
+    }
+}
+
+impl<T> Mul<Mat34<T>> for Mat4<T> where T: Number {
+    type Output = Self;
+    fn mul(self, rhs: Mat34<T>) -> Mat4<T> {
+        mul4x4_3x4(self, rhs)
+    }
+}
+
+impl<T> MulAssign<Mat34<T>> for Mat4<T> where T: Number {
+    fn mul_assign(&mut self, rhs: Mat34<T>) {
+        *self = mul4x4_3x4(*self, rhs)
     }
 }
 
@@ -243,10 +367,7 @@ mat_impl!(Mat34, 3, 4, 12, Vec4 {x, 0, y, 1, z, 2, w, 3}, Vec3 {x, 0, y, 1, z, 2
 //  from
 
 // mul 
-//  4x4 * 4x3
-//  4x3 * 4x4
-// VecN
-// mul assign
+//  VecN
 
 // transpose
 // inverse
